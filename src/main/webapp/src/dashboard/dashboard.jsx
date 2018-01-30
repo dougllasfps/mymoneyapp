c3c20b8e7c231faadc7d7b16f60b82b1864b423a
Dougllas Sousa <dougllasfps@gmail.com>
2018-01-30 06:32:00 -0300

    vai

---
+++ src/main/webapp/src/dashboard/dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>
+            DashBoard
+        </h1>
+    </div>
+)
--- src/main/webapp/src/main/app.jsx
+++ src/main/webapp/src/main/app.jsx
@@ -1,8 +1,18 @@
 import '../common/template/dependencies'
 import React from 'react'
 
+import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/rodape'
+import Routes from './routes'
+
 export default props => (
     <div className='wrapper'>
-      App
+      <Header />
+      <SideBar />
+        <div className="content-wrapper">
+          <Routes />
+        </div>
+      <Footer />
     </div>
 )
