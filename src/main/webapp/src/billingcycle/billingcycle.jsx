eaebef8e4c6fac7fc74e0cbd704a957653ef10f9
Dougllas Sousa <dougllasfps@gmail.com>
2018-02-11 18:29:24 -0300

    configurando tabs do ciclo de pagamentos.

--- src/main/webapp/src/billingcycle/billingcycle.jsx
+++ src/main/webapp/src/billingcycle/billingcycle.jsx
@@ -6,7 +6,7 @@ import Tabs from '../common/tab/tabs'
 import TabsContent from '../common/tab/tabsContent'
 
 import TabsHeader from '../common/tab/tabsHeader'
-
+import TabHeader from '../common/tab/tabHeader'
 
 export default class BillingCycles extends React.Component{
     render(){
@@ -16,6 +16,10 @@ export default class BillingCycles extends React.Component{
                 <Content>
                     <Tabs>
                         <TabsHeader>
+                            <TabHeader label="Listar" icon="bars" target="tabList" />
+                            <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+                            <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
 
                         </TabsHeader>
                         <TabsContent></TabsContent>
---
+++ src/main/webapp/src/common/tab/tabHeader.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+import {selectTab} from './tabActions'
+
+class TabHeader extends React.Component{
+    render(){
+        const selected = this.props.tab.selected === this.props.target
+
+        return(
+            <li className={selected ? 'active' : ''}>
+                <a href="javascript:;"
+                   data-toggle="tab"
+                   data-target={this.props.target}
+                   onClick={() => this.props.selectTab(this.props.target)}>
+                    <i className={`fa fa-${this.props.icon}`}></i>
+                    {this.props.label}
+                </a>
+            </li>
+        )
+    }
+}
+
+const mapStateToProps = state => (
+    {tab: state.tab}
+)
+
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps) (TabHeader)
