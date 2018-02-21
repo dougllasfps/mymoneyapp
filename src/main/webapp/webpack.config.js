0f44f3ecf302bf8ea26c61c4175beb07d7ffcb80
Dougllas Sousa <dougllasfps@gmail.com>
2018-02-21 20:20:11 -0300

    Update webpack.config.js

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-
 var environment = {
     'process.env':{
         NODE_ENV : JSON.stringify('production'),
